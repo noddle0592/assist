1723946016bfa1051bb0dae5684520562e0abd6a
zhoulifa <zlf@dongsys.com>
2020-09-20 22:30:57 +0800

    first commit

---
+++ src/main/java/com/tone/assist/WinMineAssist.java
@@ -0,0 +1,201 @@
+package com.tone.assist;
+
+import com.sun.jna.Memory;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.ptr.IntByReference;
+import com.tone.lib.number.Numbers;
+import com.tone.lib.string.Pads;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static com.tone.assist.WinUserH.*;
+
+public class WinMineAssist extends JFrame {
+    private static final byte MINE_END = 0x10; // 一行雷结束了的结束符
+    private static final byte MINE = -113; // 0x8F代表雷
+    private int height; // 雷区高度
+    private byte[] byteArray; // 雷区列表
+
+    public WinMineAssist() throws HeadlessException {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("雷区：");
+        this.getContentPane().add(label, BorderLayout.WEST);
+        JTextArea textArea = new JTextArea();
+        this.getContentPane().add(textArea, BorderLayout.CENTER);
+        JPanel panel = new JPanel(new GridLayout(6, 1, 2, 5));
+        JButton btnRead = new JButton("读取");
+        btnRead.addActionListener(event -> {
+//            System.out.println("读取中...");
+            if (this.getMines() != null) {
+                StringBuilder stringBuilder = new StringBuilder("       ");
+                for (int i = 0; i < 30; i++) {
+                    stringBuilder.append(Pads.leftPad(i + 1, 2)).append("  ");
+                    if (i % 3 == 0) {
+                        stringBuilder.append(' ');
+                    }
+                }
+                stringBuilder.append('\n');
+                for (int i = 0; i < height; i++) {
+                    stringBuilder.append(Pads.leftPad(i + 1, 2)).append("   ");
+                    for (int j = 0; j < 32; j++) {
+                        byte b = byteArray[i * 32 + j];
+                        if (b == MINE_END) {
+                            break;
+                        } else {
+                            String str;
+                            switch (b) {
+                                case MINE: // 0x8F
+                                    str = "雷   ";
+                                    break;
+                                default:
+                                    str = "非   ";
+                                    break;
+                            }
+                            stringBuilder.append(str);
+                        }
+                    }
+                    stringBuilder.append('\n');
+                }
+                textArea.setText(stringBuilder.toString());
+            }
+        });
+        panel.add(btnRead);
+        JButton btnAuto = new JButton("自动扫雷");
+        btnAuto.addActionListener(event -> {
+//            System.out.println("自动扫雷...");
+            WinDef.HWND hWnd = this.getMines();
+            if (hWnd != null) {
+                // 计算坐标位置
+                final short baseX = 20;
+                final short baseY = 60;
+                final short step = 16;
+                short[] xyPosition = new short[2];
+                WinDef.WPARAM wParam = new WinDef.WPARAM(MK_LBUTTON);
+                for (int i = 0; i < height; i++) {
+                    // y坐标要在x坐标后边。由于这里使用的是字节的高位对应数字的高位
+                    // 发现一个很奇怪的问题，x和y的坐标全部被缩小到了80%。原因未知，所以这边给他放大
+                    xyPosition[1] = (short) ((baseY + step * i) / 0.8);
+                    for (int j = 0; j < 32; j++) {
+                        byte b = byteArray[i * 32 + j];
+                        if (b == MINE_END) {
+                            break;
+                        } else if (b != MINE){
+                            // 不是雷才点击
+                            xyPosition[0] = (short) ((baseX + step * j) / 0.8);
+                            /*xyPosition[0] = (short)0;
+                            xyPosition[1] = (short)100;*/
+                            WinDef.LPARAM lParam = new WinDef.LPARAM(Numbers.short2Int(xyPosition));
+                            // 这里用PostMessage和SendMessage都可以.只是PostMessage是非阻塞的，而SendMessage是阻塞的
+                            User32.INSTANCE.PostMessage(hWnd, WM_LBUTTONDOWN, wParam, lParam);
+                            User32.INSTANCE.PostMessage(hWnd, WM_LBUTTONUP, wParam, lParam);
+//                            User32.INSTANCE.SendMessage(hWnd, WM_LBUTTONUP, wParam1, lParam);
+//                            System.out.println("发送消息 " + j + " : " +  xyPosition[0] + ", " + i + " : " + xyPosition[1] + ", b = " + b + ", param = " + param);
+                        }
+                    }
+                }
+            }
+        });
+        panel.add(btnAuto);
+        // 重新开始
+        JButton btnReset = new JButton("重置");
+        btnReset.addActionListener(event -> {
+            this.sendWmCommand(510);
+        });
+        panel.add(btnReset);
+        // 切换各种等级的菜单
+        JButton btnLow = new JButton("初级");
+        btnLow.addActionListener(event -> {
+            this.sendWmCommand(0x209);
+        });
+        JButton btnMiddle = new JButton("中级");
+        btnMiddle.addActionListener(event -> {
+            this.sendWmCommand(0x20A);
+        });
+        JButton btnHigh = new JButton("高级");
+        btnHigh.addActionListener(event -> {
+            this.sendWmCommand(0x20B);
+        });
+        panel.add(btnLow);
+        panel.add(btnMiddle);
+        panel.add(btnHigh);
+        this.getContentPane().add(panel, BorderLayout.EAST);
+    }
+
+    private void sendWmCommand(int menuId) {
+        WinDef.HWND hWnd = this.getWindow();
+        if (hWnd != null) {
+            WinDef.WPARAM wParam = new WinDef.WPARAM(menuId);
+            User32.INSTANCE.PostMessage(hWnd, WM_COMMAND, wParam, null);
+        }
+    }
+
+    private WinDef.HWND getWindow() {
+        // 获取游戏窗口句柄
+        WinDef.HWND hWnd = User32.INSTANCE.FindWindow(null, "扫雷");
+        if (hWnd == null) {
+            JOptionPane.showMessageDialog(this, "打开扫雷进程失败");
+        }
+        return hWnd;
+    }
+
+    private WinDef.HWND getMines() {
+        WinDef.HWND hWnd = this.getWindow();
+        if (hWnd != null) {
+            // 通过窗口句柄获取游戏进程ID
+            IntByReference processId = new IntByReference();
+            User32.INSTANCE.GetWindowThreadProcessId(hWnd, processId);
+            // 通过进程ID拿到进程句柄
+            WinNT.HANDLE handle = Kernel32.INSTANCE.OpenProcess(WinNT.PROCESS_ALL_ACCESS, false, processId.getValue());
+            // 读取进程的内存信息
+            // 读取雷区宽高信息。0x01005334宽    0x01005338高
+            Pointer pointer = new Pointer(0x01005334); // 宽高基地址
+            Pointer bufPointer = new Memory(8);
+            Kernel32.INSTANCE.ReadProcessMemory(handle, pointer, bufPointer, 8, null);
+//            int width = bufPointer.getInt(0); // 宽度可以不需要，因为一行读取到0x10就算结束了。这样也就是知道宽度了
+            height = bufPointer.getInt(4);
+            if (height > 24) {
+                JOptionPane.showMessageDialog(this, "读取宽高信息出错，读取的高度为" + height);
+                return null;
+            }
+            // 读取雷区内存信息
+            pointer = new Pointer(0x01005361); // 雷区基地址
+            bufPointer = new Memory(height * 32); // 知道高度后，就不要读取24 * 32这么多了
+            Kernel32.INSTANCE.ReadProcessMemory(handle, pointer, bufPointer, height * 32, null);
+            byteArray = bufPointer.getByteArray(0, height * 32);
+        }
+        return hWnd;
+    }
+
+    /**
+     * 创建并显示GUI。出于线程安全的考虑，
+     * 这个方法在事件调用线程中调用。
+     */
+    private static void createAndShowGUI() {
+        // 确保一个漂亮的外观风格
+//        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        // 创建及设置窗口
+        WinMineAssist frame = new WinMineAssist();
+
+        frame.setSize(800, 600);
+
+        // 显示窗口
+//        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // 显示应用 GUI
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
---
+++ src/main/java/com/tone/assist/WinUserH.java
@@ -0,0 +1,26 @@
+package com.tone.assist;
+
+/**
+ * Winuser.h文件里面定义的按键值
+ */
+public interface WinUserH {
+    int WM_COMMAND = 0x111; // 菜单被按下的消息
+
+    int WM_MOUSEFIRST = 0x0200;
+    int WM_MOUSEMOVE = 0x0200;
+    int WM_LBUTTONDOWN = 0x0201;
+    int WM_LBUTTONUP = 0x0202;
+    int WM_LBUTTONDBLCLK = 0x0203;
+    int WM_RBUTTONDOWN = 0x0204;
+    int WM_RBUTTONUP = 0x0205;
+    int WM_RBUTTONDBLCLK = 0x0206;
+    int WM_MBUTTONDOWN = 0x0207;
+    int WM_MBUTTONUP = 0x0208;
+    int WM_MBUTTONDBLCLK = 0x0209;
+
+    int MK_LBUTTON = 1; // 左鼠标键被按下
+    int MK_RBUTTON = 2; // 右鼠标键被按下
+    int MK_SHIFT = 4; // Shift键被按下
+    int MK_CONTROL = 8; // Ctrl键被按下
+    int MK_MBUTTON = 16; // 中鼠标键被按下
+}
